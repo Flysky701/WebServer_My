f8c697a149673c76c13c78b0f44a4fb631c6f1f1
Flysky701 <18458871liu@gmail.com>
2025-03-06 17:51:40 +0800

    完成部分http解析类， 未验证， 未debug

--- include/http/httprequest.cpp
+++ include/http/httprequest.cpp
@@ -1,18 +1,82 @@
-
 #include <vector>
 #include <string>
 #include <algorithm> 
+#include <sstream>
 #include "httprequest.h"
 
 using std::string;
+using std::string_view;
 using std::vector;
 
+bool HttpRequest::parse(const char* data, size_t len){
+    std::string_view input(data, len);
+    size_t pos = 0;
 
-vector<char> HttpRequest::change(const vector<char> *orgin){
-    vector<char> data;
-    
+    while(pos < input.size() && state_ != PARSE_ERROR){
+        size_t line_end = input.find("\r\n", pos);
+        if(line_end == string::npos)break;
+
+        string_view line = input.substr(pos, line_end - pos);
+        pos = line_end + 2;
+
+
+        if(state_ == PARSE_LINE){
+            if(parse_request_line(line) == false){
+                state_ = PARSE_ERROR;
+                return false;
+            }
+            else state_ == PARSE_HEADERS;
+        }
+        else if (state_ == PARSE_HEADERS)
+        {
+            if (line.empty()){
+                state_ = PARSE_BODY;
+                return true; // 暂时不处理body
+            }
+            parse_headers(line);
+            break;
+        }
+        else
+            break;
+    }
+}
+bool HttpRequest::parse_request_line(string_view line){
+    size_t method_end = line.find(' ');
+    if(method_end == string_view::npos)
+        return  false;
+    method_ = line.substr(0, method_end);
+
+    size_t path_start = method_end + 1;
+    size_t path_end = line.find(' ', path_start);
+    if(path_end == string_view::npos)
+        return false;
+
+    size_t version_start = path_end + 1;
+    version_ = line.substr(version_start);
+
+    return method_.size() && path_.size() && version_.size();
 }
 
-bool HttpRequest::parse(vector<char> *data){
+void HttpRequest::parse_headers(string_view line){
+    size_t colon = line.find(':');
+    if(colon == string_view::npos)
+        return;
 
+    string key(line.substr(0, colon));
+    transform(key.begin(), key.end(), key.begin(), ::tolower);
+
+    string_view value = line.substr(colon + 1);
+    value.remove_prefix(std::min(value.find_first_not_of(" "), value.size()));
+    value.remove_suffix(value.size() - std::min(value.find_last_not_of(" \r") + 1, value.size()));
+
+    headers_[key] = value;
+}
+
+bool HttpRequest::keep_alive() const
+{
+    auto it = headers_.find("connection");
+    if (it != headers_.end()){
+        return it->second == "keep-alive" || (version_ == "HTTP/1.1" && it->second != "close");
+    }
+    return version_ == "HTTP/1.1";
 }
--- include/http/httprequest.h
+++ include/http/httprequest.h
@@ -6,31 +6,46 @@
 #include <cstring>
 #include <unordered_map>
 
+class HttpRequest
+{
 
-class HttpRequest{
+public:
+    enum ParseState
+    {
+        PARSE_LINE,    // 解析请求行
+        PARSE_HEADERS, // 解析头部
+        PARSE_BODY,    // 解析正文（待扩展）
+        PARSE_ERROR    // 错误状态
+    };
+    bool parse(const char *data, size_t len);
+    bool parse(const std::vector<char> &data){return parse(data.data(), data.size());}
+    bool keep_alive() const;
 
-    public:
-        enum ParseState{
-            PARSE_LINE,    // 解析请求行
-            PARSE_HEADERS, // 解析头部
-            PARSE_BODY,    // 解析正文（后续扩展）
-            PARSE_ERROR    // 错误状态
-        };
-        vector<char> change(const std::vector<char> *orgin);
-        bool parse(std::vector<char> *data);
-        bool keep_alive() const;
+    const std::string &method() const { return method_; }
+    const std::string &path() const { return path_; }
+    const std::string &version() const { return version_; }
+    const ParseState &state() const { return state_; }
 
-        const std::string &method() const { return method_; }
-        const std::string &path() const { return path_; }
-        const std::string &version() const { return version_; }
-        const ParseState &state() const { return state_; }
+private:
+    ParseState state_ = PARSE_LINE;
+    std::string method_;
+    std::string path_;
+    std::string version_;
+    std::unordered_map<std::string, std::string> headers_;
 
-    private:
-        ParseState state_ = PARSE_LINE;
-        std::string method_;
-        std::string path_;
-        std::string version_;
-        std::unordered_map<std::string, std::string> headers_;
+    bool parse_request_line(std::string_view line);
+    void parse_headers(std::string_view line);
+
+    static std::vector<char> hex_decode(const std::vector<char> orgin)
+    {
+        std::vector<char> bin;
+        for (size_t i = 0; i < orgin.size(); i += 2){
+            std::string tmp = "" + orgin[i] + orgin[i + 1];
+            bin.emplace_back(static_cast<char>(
+                std::stoi(tmp, nullptr, 16)));
+        }
+        return bin;
+    }
 };
 
 #endif
